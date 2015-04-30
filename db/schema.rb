fb9f2b23f625617cffa308983ca1214b678287e8
Mike Dai Wang <mikedaiwang@gmail.com>
2015-04-30 02:17:49 -0400

    ui changes and added name field to user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150430054353) do
+ActiveRecord::Schema.define(version: 20150430060640) do
 
   create_table "pages", force: :cascade do |t|
     t.integer  "project_id"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20150430054353) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
