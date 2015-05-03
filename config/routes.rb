083def9cb0a0e315dd3c6d1d80746df80ee88c71
Mike Dai Wang <mikedaiwang@gmail.com>
2015-05-02 22:53:27 -0400

    added demo pages

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,9 @@ Rails.application.routes.draw do
   end
   
   get 'demo', :to => 'static_pages#index'
+  get 'demo_project/:project_id', :to => 'static_pages#project'
+  get 'demo_project/:project_id/page/:id', :to => 'static_pages#page'
+  
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
